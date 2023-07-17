8af91a85bc7c16e11fab8d09ebcbcc3361ffde6f
Amin-37 <nurulaminbhuiyan1098@gmail.com>
2023-07-17 21:16:29 +0930

    Todo feature added
    Todo feature added

--- NurulAmin_S358037/Week1/HellowWorld/Program.cs
+++ NurulAmin_S358037/Week1/HellowWorld/Program.cs
@@ -1,20 +1,9 @@
-using HellowWorld.Data;
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using BlazorApp1.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -28,13 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
