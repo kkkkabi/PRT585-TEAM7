4a613e827f32c6e1a92f84de21172e57aa200970
kkkkabi <sinmei_li@hotmail.com>
2023-08-05 21:52:04 +0930

    week3 update

---
+++ Kabi Li_S360233/week3/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+        public IActionResult second()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["Numtimes"] = numTimes;
+            return View();
+        }
+    }
+}
+
